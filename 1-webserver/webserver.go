3aa2276b76b7feab23266d7b8c1d05be9ff02c60
Jake Shasteen <jakeshasteen@gmail.com>
2020-07-28 06:14:15 -0700

    Add webserver

---
+++ 1-webserver/webserver.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", anyRoute)
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+func anyRoute(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+}
