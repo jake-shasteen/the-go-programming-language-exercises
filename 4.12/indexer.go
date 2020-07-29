ce5be8651c5cc5c4e25512415b2317e89259b588
Jake Shasteen <jakeshasteen@gmail.com>
2020-07-29 13:47:57 -0700

    Unmarshal the files into memory

--- 4.12/indexer.go
+++ 4.12/indexer.go
@@ -13,15 +13,67 @@ package main
 import (
 	"os"
 	"fmt"
+	"encoding/json"
+	"io/ioutil"
+	"strings"
+	"strconv"
 )
 
+type XKCDJSON struct {
+	Month int `json:",string"`
+	Num int 
+	Link string
+	Year int `json:",string"`
+	News string
+	SafeTitle string `json:"safe_title"`
+	Transcript string
+	Alt string
+	Img string
+	Title string
+	Day int `json:",string"`
+}
+
 func main () {
+	// searchIndex := make(map[string]([]int))
+	comicIndex := make(map[int]*XKCDJSON)
+
 	dir, err := os.Open("xkcd")
-	names, err := dir.Readdirnames(0)
+	handle(err)
+	fileNames, err := dir.Readdirnames(0)
+	handle(err)
+	dir.Close()
+
+	for _, fileName := range fileNames {
+		fmt.Printf("%s read\n", fileName)
+
+		file, err := os.Open("xkcd/" + fileName)
+		handle(err)
+
+		b, err := ioutil.ReadAll(file)
+		handle(err)
+
+		file.Close()
+
+		index, err := strconv.Atoi(strings.Split(fileName, ".")[0])
+		handle(err)
+
+		// var tmpXKCD XKCDJSON
+
+		comicIndex[index] = new(XKCDJSON) // allocate space for XKCDJSON
+		err = json.Unmarshal(b, comicIndex[index])
+		handle(err)
+
+		// *(comicIndex[index]) = tmpXKCD
+
+	}
+
+	for index, json := range comicIndex {
+		fmt.Printf("%d: %s \n", index, (*json).SafeTitle)
+	}
+}
+
+func handle(err error) {
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
-	}
-	for _, name := range names {
-		fmt.Printf("%s\n", name)
 	}
 }
