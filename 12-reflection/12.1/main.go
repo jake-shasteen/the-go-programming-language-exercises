eb0000dafb3f54d288c46c560951363cc2987f9a
Jake Shasteen <jakeshasteen@gmail.com>
2021-02-07 00:15:19 -0800

    Display struct and array map keys

--- 12-reflection/12.1/main.go
+++ 12-reflection/12.1/main.go
@@ -6,8 +6,34 @@ import (
 	"strconv"
 )
 
-func main() {
+type Foo struct {
+	bar string
+	baz int
+}
 
+func main() {
+	blar := make(map[Foo]int)
+	blar2 := make(map[[3]int]int)
+
+	intArray := [3]int{10, 12, 14}
+
+	one := &Foo{bar: "one", baz: 1}
+	two := &Foo{bar: "two", baz: 2}
+	three := &Foo{bar: "three", baz: 3}
+
+	blar[*one] = 1
+	blar[*two] = 2
+	blar[*three] = 3
+
+	blar2[intArray] = 16
+
+	Display("foo", blar)
+	Display("bar", blar2)
+}
+
+func Display(name string, x interface{}) {
+	fmt.Printf("Display %s (%T):\n", name, x)
+	display(name, reflect.ValueOf(x))
 }
 
 // Extend `Display` so that it can display maps whose keys are structs or arrays
@@ -16,17 +42,19 @@ func display(path string, v reflect.Value) {
 	case reflect.Invalid:
 		fmt.Printf("%s = invalid\n", path)
 	case reflect.Slice, reflect.Array:
-		for i := 0; i < v.Len(); i++ {
+		length := v.Len()
+		for i := 0; i < length; i++ {
 			display(fmt.Sprintf("%s[%d]", path, i), v.Index(i))
 		}
 	case reflect.Struct:
-		for i := 0; i < v.NumField(); i++ {
-			fieldPath = fmt.Sprintf("%s.%s", path, v.Type().Field(i).Name)
+		length := v.NumField()
+		for i := 0; i < length; i++ {
+			fieldPath := fmt.Sprintf("%s.%s", path, v.Type().Field(i).Name)
 			display(fieldPath, v.Field(i))
 		}
 	case reflect.Map:
 		for _, key := range v.MapKeys() {
-			display(fmt.Sprintf("%s[%s]", path, formatAtom(key)), v.MapIndex(key))
+			display(fmt.Sprintf("%s[%s]", path, formatComparable(key) /*the rendered key*/), v.MapIndex(key))
 		}
 	case reflect.Ptr:
 		if v.IsNil() {
@@ -44,6 +72,46 @@ func display(path string, v reflect.Value) {
 	default:
 		fmt.Printf("%s = %s\n", path, formatAtom(v))
 	}
+}
+
+// formatComparable formats Comparable type variables
+// https://golang.org/ref/spec#Comparison_operators
+func formatComparable(v reflect.Value) string {
+	switch v.Kind() {
+	case reflect.Ptr:
+		if v.IsNil() {
+			return "nil"
+		} else {
+			return fmt.Sprintf("%x", v.Pointer())
+		}
+	case reflect.Slice, reflect.Array:
+		var result string
+		length := v.Len()
+		result = result + "["
+		for i := 0; i < length; i++ {
+			result = result + formatAtom(v.Index(i))
+			if i != length-1 {
+				result = result + ", "
+			}
+		}
+		result = result + "]"
+		return result
+	case reflect.Struct:
+		var result string
+		length := v.Type().NumField()
+		result = result + "{"
+		for i := 0; i < length; i++ {
+			result += v.Type().Field(i).Name + ": " + formatAtom(v.Field(i))
+			if i != length-1 {
+				result = result + ","
+			}
+		}
+		result = result + "}"
+		return result
+	default:
+		return formatAtom(v)
+	}
+
 }
 
 // formatAtom formats a value without inspecting its internal structure.
